cbb96c3488805d14f7935a6d39e2754404d2393c
Einz <jason199786109@gmail.com>
2019-12-09 19:42:49 +0800

    Readjust code to increase readability.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#%%
 from mylib.som import SOM
 from mylib.data import Data
 from mylib.kdtree import KdTree
-#%%
+
 import numpy as np
-#%%
-data = Data()
-X, y = data.clean()
 
-#%%
-som = SOM(X, y)
-som.train(width=100, height=100, epochs=1e4)
 
-# %%
-som.show(count=1000)
-
-# %%
+# Start from here!
+if(__name__ == "__main__"):
+    # Get Data & Data Preprocessing
+    data = Data()
+    X, y = data.clean()
+    # Training SOM model & get Result
+    som = SOM(X, y)
+    som.train(width=100, height=100)
+    new_data = som.get_map()
+    # Apply KdTree
+    kdtree = KdTree()
--- mylib/som.py
+++ mylib/som.py
@@ -1,35 +1,36 @@
+import numpy as np
 from minisom import MiniSom
 import matplotlib.pyplot as plt
 
 
 class SOM:
-    def __init__(self, data, target):
-        self.data = data
-        self.nfeatures = data.shape[1]
-        self.nrow = data.shape[0]
+    def __init__(self, features, target):
+        self.features = features
+        self.nfeatures = features.shape[1]
+        self.nrow = features.shape[0]
         self.target = target
         self.markers = ['x', 'o', 'D', '*', '1', 'v', '.', 's']
         self.colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
     #  Training SOM model
-    def train(self, width=10, height=10, sigma=3, lr=.5, neighborhood_func='triangle', epochs=1e5):
-        self.som = MiniSom(width, height, self.nfeatures, sigma=sigma, learning_rate=lr, neighborhood_function=neighborhood_func)
-        self.som.train_random(self.data, int(epochs), verbose=True)
+    def train(self, width=10, height=10, sigma=.9, lr=.2, epochs=1e5):
+        self.som = MiniSom(width, height, self.nfeatures, sigma=sigma, learning_rate=lr)
+        self.som.train_random(self.features, int(epochs), verbose=True)
     
     # Get the SOM results
     def get_map(self, verbose=True, log=1000):
         out = []
-        for step, X in enumerate(self.data):
-            new_pos = self.som.winner(X)
-            out.append((new_pos, X))
+        for step, (X, y) in enumerate(zip(self.features, self.target)):
+            new_X = self.som.winner(X)
+            out.append((new_X, X, y))
             if(verbose == True and step % log == 0):
                 print(f'*Creating SOM: [{step}/{self.nrow}]')
-        return out
+        return np.array(out)
 
     # Plot the SOM
     def show(self, verbose=True, log=1000):
-        for step, ((som_pos, _), y) in enumerate(zip(self.get_map(verbose, log), self.target)):
-            plt.plot(som_pos[0], som_pos[1], self.markers[int(y)], color=self.colors[int(y)])
+        for step, (new_X, _, y) in enumerate(self.get_map(verbose, log)):
+            plt.plot(new_X[0], new_X[1], self.markers[int(y)], color=self.colors[int(y)])
             if(verbose == True and step % log == 0):
                 print(f'*Plotting SOM: [{step}/{self  .nrow}]')
         plt.show()
